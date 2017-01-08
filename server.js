033c08c00988494dc3abe6a89e8454a6d3dddb4e
Osman Cea <osman.cea@gmail.com>
2017-01-08 15:24:15 -0300

    timestamp microservice

--- server.js
+++ server.js
@@ -5,62 +5,82 @@ var app = express();
 
 app.get('/:query', function (req, res) {
 
-	// get the url query
-	var url = req.params.query;
-	
 	// response by default
 	var response = {
 		unix: null,
 		natural: null
 	};
 
+	// get the url query
+	var url = req.params.query;
 
-	// full name of months
-	
 	// if url is timestamp
 	if ( isNumeric(url)) {
 		
 		response.unix = parseInt(url);
 
-		var objDate = new Date(parseInt(url));
-
-		response.natural = parseDateString(objDate);
-
-		res.send(response);
 	} 
 	// if url is natural datestring
 	else if ( (new Date(url)).getTime() > 0 ) {
-		response.unix = (new Date(url)).getTime();
-		response.natural = url.split("%20").join(" ");
 
-		res.send(response);
+		response.unix = getTimeStamp(new Date(url));
+
 	}
-	// if is invalid or whatever
-	else {
-		res.send(response);
-	}
+
+	response.natural = parseDateString(response.unix);
+	
+	res.send(response);
+
 });
 
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+  console.log('Timestamp microservice listening on port 3000!');
+
 });
+
+
+
+/****
+*
+*	Helpers
+*
+****/
+
+
+var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 
 // check if value is numeric
 function isNumeric(num){
     return !isNaN(num)
 }
 
-var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-// parse data to string
-function parseDateString(objDate) {
+// from timestamp to string
+function parseDateString(timestamp) {
 
-	console.log(objDate);
+	if (!timestamp) return;
+
+	var timezoneOffset = (new Date()).getTimezoneOffset()*60;
+
+	var objDate = new Date((timestamp + timezoneOffset)*1000);
+
+
 	
- 	var month = monthNames[objDate.getMonth()];
-	var day = objDate.getDate();
-	var year = objDate.getFullYear();
+ 	var month 	= monthNames[objDate.getMonth()], 
+ 		day 	= objDate.getDate(), 
+		year 	= objDate.getFullYear();
 
 	return (month + " " + day + ", " + year);
+}
+
+
+// from date to timestamp
+function getTimeStamp(objDate) {
+
+	if (!objDate) return;
+
+	return objDate.getTime()/1000 - objDate.getTimezoneOffset()*60;
 }
